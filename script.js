eaf873b93eb6709fe28cf642db41d336ca8c0d2e
ruffaaaaa <aranez.ruffa@gmail.com>
2023-03-16 16:52:06 +0800

    4thcomm

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+
+let url = location.href;
+
 function login() {
     const username = document.getElementsByName('username')[0].value;
     const password = document.getElementsByName('password')[0].value;
 
     if (username == "ruffa" && password == "cutie") {
         alert("Login Successfully");
+        window.location="home.html";
     } 
     else if ( username !== "" && password !== "") {
         alert("Invalid email or password. Please try again.");
